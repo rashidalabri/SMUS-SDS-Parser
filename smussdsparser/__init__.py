5c4a3d3e8501a582b1d7fde7f1fb035af73fef69
rashidx <itsrashidalabri@gmail.com>
2017-12-25 01:59:48 -0800

    Added __all__ variable.

--- smussdsparser/__init__.py
+++ smussdsparser/__init__.py
@@ -2,3 +2,5 @@ from .student import Student
 from .course import Course
 from .session import Session
 from .assignment_mark import AssignmentMark
+
+__all__ = ['student', 'course', 'session', 'assignment_mark']
