b5cb87ed79719dcc8926ce13d8f508f07c80243f
rashidx <itsrashidalabri@gmail.com>
2017-12-25 22:40:22 -0800

    Added a "not logged in" exception for error handling and updated the example code to reflect new changes.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SMUS SDS Parser',
+    version='v0.1.1-alpha',
+    packages=['smussdsparser'],
+    url='https://github.com/rashidx/SMUS-SDS-Parser',
+    license='MIT',
+    author='Rashid Al-Abri',
+    author_email='itsrashidalabri@gmail.com',
+    description='A python library that parses the St. Michaels University SDS student panel.'
+)
